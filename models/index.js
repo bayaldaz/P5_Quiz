9c6b478fa9bde1ea121d27efb33eb52db0046065
bayaldaz <bayaldaz@gmail.com>
2018-05-08 18:03:21 +0200

    master

---
+++ models/index.js
@@ -0,0 +1,23 @@
+var quiz = require("./quiz");
+
+exports.quiz = quiz;
+
+const path = require('path');
+
+//Load ORM
+const Sequelize = require('sequelize');
+
+//To use SQLite data base
+const sequelize = new Sequelize("sqlite:quizzes.sqlite");
+
+//Import the definitiion of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname,'quiz'));
+
+//Create tables
+sequelize.sync()
+.then(() => console.log('Data Bases created succesfully'))
+.catch(error => {
+    console.log("Error creating the data base tables:", error);
+    process.exit(1);
+});
+module.exports = sequelize;
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const quizController = require('../controllers/quiz');
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+    res.render('index');
+});
+
+/* GET credits page */
+router.get('/credits', (req, res, next) => {
+    res.render('credits');
+});
+
+router.param('quizId', quizController.load);
+
+/* GET quizzes page */
+router.get('/quizzes', quizController.index);
+
+module.exports = router;
